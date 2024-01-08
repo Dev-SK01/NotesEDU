4a21d2c5adce09d3cf331b6e5d3dca39bbbd6eb6
Dev-SK01 <srik3713@gmail.com>
2024-01-08 17:24:06 +0530

    Saving function added

--- notes-edu/src/JournalNote.js
+++ notes-edu/src/JournalNote.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './journal.css';
 
 const JournalNote = () => {
+
+    const [journalData, setJournalData] = useState([
+        {
+            id: new Date().getTime() + 1,
+            date: new Date().toDateString(),
+            note: `I am So Proud
+                i am good , i'm sooo good
+                good good`
+        },
+        {
+            id: new Date().getTime() + 2,
+            date: new Date().toDateString(),
+            note: `I am So Proud
+                i am good , i'm sooo good
+                good good`
+        }, {
+            id: new Date().getTime() + 3,
+            date: new Date().toDateString(),
+            note: `I am So Proud
+                i am good , i'm sooo good
+                good good`
+        },
+    ] || [])
+    const showNotes = (e) => {
+        // console.dir(e.target.parentNode.nextSibling)
+        if (e.target.parentNode.nextSibling.style.display == 'block') {
+            e.target.parentNode.nextSibling.style.display = 'none';
+        } else {
+            e.target.parentNode.nextSibling.style.display = 'block';
+        }
+    }
+    const handleSave = (e, id) => {
+        const ExistingData = journalData.filter((journal) => {
+            if (journal.id != id) {
+                return journal
+            }
+        })
+        console.log(ExistingData)
+       const EditedData = journalData.filter((data) => {
+            let Edited = e.target.parentNode.nextSibling.value;
+            if (data.id == id) {
+                data.note = Edited;
+                return data
+            }
+        })
+        console.log(EditedData)
+        localStorage.setItem('journalData' , JSON.stringify([...ExistingData,EditedData]))
+    }
     return (
         <>
 
@@ -10,21 +58,33 @@ const JournalNote = () => {
             </div>
             <div className="container">
                 <div className="journal-container">
-                    <div className="journal">
-                        <p>{new Date().toDateString()}</p>
-                        <textarea
-                            name="textarea"
-                            id={
-                                new Date().toLocaleDateString().slice(0,5).replace('/','-')
-                            }
-                            cols="20"
-                            rows="10"
-                        >
-
-                        </textarea>
+                    <div div className="journal">
+                        {journalData.map((data) => (
+                            <>
+                                <div className='title'>
+                                    <p onClick={(e) => showNotes(e)}>{data.date}</p>
+                                    <i
+                                        className="bi bi-check2-circle save"
+                                        onClick={(e) => handleSave(e, data.id)}
+                                    ></i>
+                                </div>
+                                <textarea
+                                    name="textarea"
+                                    id={data.id}
+                                    cols="20"
+                                    rows="10"
+                                    autoCapitalize='on'
+                                    autoCorrect='on'
+                                    placeholder='Enter Your Notes Here....'
+                                    key={data.id}
+                                >
+                                    {data.note}
+                                </textarea>
+                            </>
+                        ))}
                     </div>
                 </div>
-            </div>
+            </div >
 
         </>
     )
