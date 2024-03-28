4cef8af047b208916b19eb7d2b6588d97900f345
Dev-SK01 <srik3713@gmail.com>
2024-03-28 23:25:09 +0530

    UI Updated | Header Animation added

--- notes-edu/src/Header.js
+++ notes-edu/src/Header.js
@@ -2,37 +2,41 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './css/App.css'
 const Header = () => {
-    const [quote , setQuote] = useState('Loading......');
+    const [quote, setQuote] = useState('Loading......');
     console.log(quote);
-     async function getQuotes(){
-        try{
+    async function getQuotes() {
+        try {
             const API_URL = 'https://api.adviceslip.com/advice'
             const response = await fetch(API_URL);
             const data = await response.json();
             // console.log(data.slip.advice)
             setQuote(data.slip.advice);
-        }catch(err){
+        } catch (err) {
             setQuote(err);
         }
-       
+
     }
     getQuotes();
     return (
-            <section className='main-nav'>
-                <div className="celebration" id='celebration'>
+        <section className='main-nav'>
+            <div className="celebration" id='celebration'>
 
-                </div>
-                <header className='header'>
-                    <h1 style={{ fontSize: '2rem', fontWeight: 'bolder' }}>Notes Edu</h1>
-                </header>
-                <div className="router">
-                    <p><Link to='NotesEDU/journal'>Journal APP</Link></p>
-                    <p><Link to='NotesEDU/todo'>Todo APP</Link></p>
-                </div>
-               <p className="quote">{quote}</p>
+            </div>
+            {/* <h1 style={{ fontSize: '2rem', fontWeight: 'bolder' }}>Notes Edu</h1> */}
+            <svg>
+                <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+                    Notes EDU
+                </text>
+            </svg>
 
-            </section>
-        
+            <div className="router">
+                <p><Link to='NotesEDU/journal'>Journal APP</Link></p>
+                <p><Link to='NotesEDU/todo'>Todo APP</Link></p>
+            </div>
+            <p className="quote">{quote}</p>
+
+        </section>
+
     )
 }
 
